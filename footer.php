ef3d4ef8c2f00270bc29b43212e7d83ef5ce55c4
Felix Milea-Ciobanu <felixmilea@gmail.com>
2013-06-03 14:57:30 -0600

    basic implementation of responsiveness

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 	</div>
 	<footer id="footer" class="wrapper">
 		<p class="left-column">&copy; <?php echo date("Y"); ?> <?php bloginfo('name'); ?></p>
-		<?php wp_nav_menu( array( 'container_class' => 'right-column', 'theme_location' => 'footer' ) ); ?>
+		<?php wp_nav_menu( array( 'container_class' => 'right-column container', 'theme_location' => 'footer' ) ); ?>
 	</footer>
 		<?php wp_footer(); ?>
 </body>
